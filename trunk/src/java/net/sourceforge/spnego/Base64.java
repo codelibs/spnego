db8184b17b30905df02175542ba2f5dfb525b816
darwinfelix <darwinfelix@2dc27796-2352-4f2f-93cc-67437fbd1425>
2010-10-07 21:07:08 +0000

    initial check-in of changes to support SOAPAction as an HTTP header parameter.

--- trunk/src/java/net/sourceforge/spnego/Base64.java
+++ trunk/src/java/net/sourceforge/spnego/Base64.java
@@ -1,9 +1,23 @@
 package net.sourceforge.spnego;
 
-/**
- * Base64 encoder/decoder. 
+/* Encodes and decodes to and from Base64 notation.
+ * Copyright (C) 2003 "Eric Glass" <jcifs at samba dot org>
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 public final class Base64 {
 
     private static final String ALPHABET =
