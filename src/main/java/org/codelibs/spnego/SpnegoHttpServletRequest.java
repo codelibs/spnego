990060af235e61488dfdbe3b54ffad76e96f161c
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2016-07-11 22:46:02 +0900

    change to public

--- src/main/java/org/codelibs/spnego/SpnegoHttpServletRequest.java
+++ src/main/java/org/codelibs/spnego/SpnegoHttpServletRequest.java
@@ -39,7 +39,7 @@ import org.ietf.jgss.GSSCredential;
  * @author Darwin V. Felix
  *
  */
-final class SpnegoHttpServletRequest extends HttpServletRequestWrapper 
+public final class SpnegoHttpServletRequest extends HttpServletRequestWrapper 
     implements DelegateServletRequest {
     
     /** Client Principal. */
